553f355fda90bb86b8e6cceee98d3c6385ef999e
GORO <GORO@HOME-PC>
2025-11-24 12:53:11 +0200

    Добавьте файлы проекта.

---
+++ Api/Models/Artist.cs
@@ -0,0 +1,38 @@
+﻿namespace Models
+{
+
+    public class ArtistObject
+    {
+        public Artists artists { get; set; }
+    }
+
+    public class Artists
+    {
+        public string href { get; set; }
+        public int limit { get; set; }
+        public string next { get; set; }
+        public int offset { get; set; }
+        public object previous { get; set; }
+        public int total { get; set; }
+        public Artist[] items { get; set; }
+    }
+
+    public class Artist
+    {
+        public External_Urls external_urls { get; set; }
+        public Followers followers { get; set; }
+        public object[] genres { get; set; }
+        public string href { get; set; }
+        public string id { get; set; }
+        public Image[] images { get; set; }
+        public string name { get; set; }
+        public int popularity { get; set; }
+        public string type { get; set; }
+        public string uri { get; set; }
+    }
+    public class Followers
+    {
+        public object href { get; set; }
+        public int total { get; set; }
+    }
+}
